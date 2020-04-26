01ab331d46ad0fb28edcc968004da2416527b78a
Yevhenii Alieksandrov <yeva@ciklum.com>
2020-04-26 18:14:14 +0300

    fix some bugs

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,21 @@
-import ScrollMagic from "scrollmagic";
-import gsap, { Power2 } from 'gsap';
+import { gsap, Power2 } from "gsap";
+import * as ScrollMagic from "scrollmagic";
+import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
 
-gsap.registerPlugin();
+ScrollMagicPluginGsap(ScrollMagic, gsap);
+
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
 	// Init Controller
-	controller = new ScrollMagic.Controller({addIndicators: true});
+	controller = new ScrollMagic.Controller();
 	// Select elements
 	const sliders = document.querySelectorAll('.slide');
 	const nav = document.querySelector('.nav-header');
 	// Loop over each slide
-	sliders.forEach(slide => {
+	sliders.forEach((slide, index, slides) => {
 		const revealImg = slide.querySelector('.reveal-img');
 		const img = slide.querySelector('img');
 		const revealText = slide.querySelector('.reveal-text');
@@ -27,16 +30,26 @@ function animateSlides() {
 		// Create scene
 		slideScene = new ScrollMagic.Scene({
 			triggerElement: slide,
-			triggerHook: 0.25
+			triggerHook: 0.25,
+			reverse: false
 		})
-		.setTween(slide)
-		.addIndicators({
-			colorStart:'white',
-			colorTrigger: 'white',
-			name: 'slide'
+		.setTween(slideTl)
+		.addTo(controller);
+		// New Animation
+		const pageTl = gsap.timeline();
+		let nextSlide= slide.length - 1 === index ? 'end' : slide[index++];
+		pageTl.fromTo(nextSlide, { y: '0%'}, {y: '50%'});
+		pageTl.fromTo(slide, {opacity: 1, scale: 1}, {opacity: 0, scale:0.5});
+		pageTl.fromTo(nextSlide, { y: '50%'}, {y: '0%'}, '-=0.5');
+		// Creata new scene
+		pageScene = new ScrollMagic.Scene({
+			triggerElement: slide,
+			duration: '100%',
+			triggerHook: 0
 		})
+		.setPin(slide, {pushFollowers: false})
+		.setTween(pageTl)
 		.addTo(controller)
 	})
 }
-
 animateSlides();
